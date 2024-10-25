433f85a6fce9264ff7045af856206a524b750c77
Afanasev Mikhail <miafanasev@gmail.com>
2024-10-25 22:37:36 +0700

    Первые пробы

--- src/main/java/ru/innotech/Main.java
+++ src/main/java/ru/innotech/Main.java
@@ -1,4 +1,33 @@
 package ru.innotech;
 
+import java.util.concurrent.*;
+
 public class Main {
+    public static void main(String[] args) {
+       // ExecutorService service = Executors.newFixedThreadPool(4);
+//        ExecutorService service = new ThreadPoolExecutor(5, 10, 1L, TimeUnit.MINUTES, new ArrayBlockingQueue<>(10));
+//        for (int i = 0; i < 10; i++) {
+//            final int w = i + 1;
+//            service.execute(() -> {
+//                System.out.println(w + " - begin");
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e ) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println(w + " - end");
+//            });
+//        }
+        //service.shutdown();
+        //service.awaitTermination(); // ждать с таймаутом
+        //Runnable t = new Thread();
+
+
+    MyThreadPool t = new MyThreadPool(1);
+    t.execute(()-> System.out.println("задание 1"));
+//        t.execute(()-> System.out.println("Поток1"));
+//      t.execute(()-> System.out.println("Поток2"));
+//        System.out.println(t.tasks);
+
+    }
 }
--- src/main/java/ru/innotech/MyThreadPool.java
+++ src/main/java/ru/innotech/MyThreadPool.java
@@ -2,7 +2,49 @@ package ru.innotech;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MyThreadPool {
-    List<Runnable> threads = new LinkedList<>(); // очередь задач на исполнение
+    List<Runnable> tasks = new LinkedList<>(); // очередь потоков на исполнение
+    Queue<Thread> threadPoll;   // очередь потоков
+    Lock monitor = new ReentrantLock();
+
+    public MyThreadPool(int capacity) { // конструктор с ёмкостью
+        if (capacity < 1 ) throw new IllegalArgumentException("Размер пула потоков не может быть меньше одного");
+        threadPoll = new ArrayBlockingQueue(capacity);
+        //monitor.lock(); // сразу блокируем монитор
+        //monitor.notify();
+        for(int i = 0; i < capacity; i++) {
+            int w = i;
+            threadPoll.add(new Thread(()->
+            {
+                while(true) {
+                    System.out.println("Поток " + w);
+                    monitor.lock();
+                    try {
+                        if(!tasks.isEmpty()) {
+                            tasks.get(0).run(); // выполняем метод из очереди
+                        }
+                        monitor.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }));}
+        for (Thread r : threadPoll) {
+            r.start();
+        }
+
+    }
+
+    public void execute(Runnable command) {    // добавление потока в очередь и запуск на исполнение, если в пуле есть место
+        if (command == null) throw new NullPointerException();
+        tasks.add(command);
+        monitor.lock();
+        monitor.notify();
+    }
+
 }
