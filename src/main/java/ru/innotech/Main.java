e1bc6d11d07e5c39ca4c0abc086cd09f90378fd0
Afanasev Mikhail <miafanasev@gmail.com>
2024-11-05 22:38:49 +0700

    Главный класс для теста

--- src/main/java/ru/innotech/Main.java
+++ src/main/java/ru/innotech/Main.java
@@ -3,31 +3,25 @@ package ru.innotech;
 import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) {
-       // ExecutorService service = Executors.newFixedThreadPool(4);
-//        ExecutorService service = new ThreadPoolExecutor(5, 10, 1L, TimeUnit.MINUTES, new ArrayBlockingQueue<>(10));
-//        for (int i = 0; i < 10; i++) {
-//            final int w = i + 1;
-//            service.execute(() -> {
-//                System.out.println(w + " - begin");
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (InterruptedException e ) {
-//                    e.printStackTrace();
-//                }
-//                System.out.println(w + " - end");
-//            });
-//        }
-        //service.shutdown();
-        //service.awaitTermination(); // ждать с таймаутом
-        //Runnable t = new Thread();
+    public static void main(String[] args) throws InterruptedException {
+        MyThreadPool t = new MyThreadPool(4);
 
+        for(int i = 0; i < 10; i++) {
+            int e = i;
+            t.execute(() -> {System.out.println("Задание " + e + " исполнено");
+            });
+        }
 
-    MyThreadPool t = new MyThreadPool(1);
-    t.execute(()-> System.out.println("задание 1"));
-//        t.execute(()-> System.out.println("Поток1"));
-//      t.execute(()-> System.out.println("Поток2"));
-//        System.out.println(t.tasks);
+        Thread.sleep(2000);
 
+        for(int i = 10; i < 20; i++) {
+            int e = i;
+            t.execute(() -> System.out.println("Задание " + e + " исполнено"));
+        }
+
+        t.shutdown();
+        //t.awaitTermination();
+
+        System.out.println("Конец главного потока");
     }
 }
